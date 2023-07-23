a759cd4ca68b1a07099c87df399334a4e2947a7e
PC <joao.caglianone@gmail.com>
2023-07-23 10:48:15 -0300

    teste

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Função para limpar o conteúdo do objeto
+// function limparObjeto() {
+//     var objeto = document.getElementById("cabecalho");
+//     if (objeto) {
+//         while (objeto.firstChild) {
+//             objeto.removeChild(objeto.firstChild);
+//         }
+//     }
+// }
+// window.addEventListener("beforeunload", function (tag){
+//         let remocao = document.getElementsByTagName(tag)[0]
+//         // remocao.remove()
+//         alert(123)
+// })
+//     //
+//     // remove("object")
+//     // remove("object")
+//
+// function handleClick(event) {
+//     var target = event.target;
+//     // Coloque aqui a lógica da sua função a ser executada
+//     console.log("Clicou em: " + target.tagName);
+// }
+//
+// window.addEventListener("DOMContentLoaded", function() {
+//     var divElement = document.getElementsByTagName("header")[0];
+//     divElement.addEventListener("click", handleClick);
+// });
+
+function limparObjeto(){
+    let remocao = document.getElementById("cabecalho")
+    alert(remocao)
+    remocao.remove()
+}
